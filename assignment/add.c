36762a8953440f36db1b389c78112003f403d27d
TUSHAR49-CPU <tanmaykhade9124@gmail.com>
2026-07-06 15:35:34 +0530

    Add files via upload

---
+++ assignment/add.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+	int a=10,b=50,res;
+	res=a+b;
+	printf("%d",res);
+}
---
+++ assignment/tri.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+	int b=5,h=9,area;
+	area=b*h/2;
+	printf("%d",area);
+	
+}
